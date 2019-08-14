076bbbab9b62e77f83db550d85b5b266aea1a6eb
Secund <Secund@SECUND-PC>
2019-08-14 14:02:21 +0300

    "Bug" fix - if more than 1 planets were placed, previous planets would stop orbiting and their state would all be messed up.
    After some research, the fault was that I was handing pointers to Planet[0] from the main PlanetVector. But as its size grew, it reallocated its elements, leaving Planet[0] with an invalid pointer.
    
    Implemented solution was to use new allocation of planets and use NewPointer-Handles to keep track of vector elements.

--- PixelPlayGround/Planet.h
+++ PixelPlayGround/Planet.h
@@ -25,7 +25,6 @@ public:
 		fTicker {1.0f}
 	{}
 
-
 public:
 
 	void draw();
--- PixelPlayGround/main2.cpp
+++ PixelPlayGround/main2.cpp
@@ -43,12 +43,12 @@ public:
 
 
 		
-		for (Planet& plnt : vTotalPlanets) {
-			plnt.update(fElapsedTime);
-			plnt.draw();
+		for (Planet* plnt : vTotalPlanets) {
+			plnt->update(fElapsedTime);
+			plnt->draw();
 		}
 	
-			checkMouse();
+		checkMouse();
 
 
 		return true;
@@ -64,7 +64,7 @@ public:
 
 	// DEMO Data Members
 private:
-	vector<Planet> vTotalPlanets;
+	vector<Planet*> vTotalPlanets;
 };
 
 
@@ -107,7 +107,7 @@ int main() {
 
 void Demo::createPlanet()
 {
-	vTotalPlanets.push_back(Planet{ *this, 10, Vec2{ float(GetMouseX() - 10), float(GetMouseY() - 10) }, olc::Pixel {uint8_t (rand() % 255), uint8_t(rand() % 255), uint8_t(rand() % 255) } });
+	vTotalPlanets.push_back(new Planet{ *this, 10, Vec2{ float(GetMouseX() - 10), float(GetMouseY() - 10) }, olc::Pixel {uint8_t (rand() % 255), uint8_t(rand() % 255), uint8_t(rand() % 255) } });
 
 }
 
@@ -147,8 +147,8 @@ void Demo::checkMouse()
 
 			if (vTotalPlanets.size() > 1) {			
 
-				Planet& last_created_planet = vTotalPlanets[vTotalPlanets.size() - 1];
-				vTotalPlanets[0].attachPlanet(last_created_planet);
+				Planet& last_created_planet = *vTotalPlanets[vTotalPlanets.size() - 1];
+				vTotalPlanets[0]->attachPlanet(last_created_planet);
 			}
 		}
 	}
@@ -156,8 +156,8 @@ void Demo::checkMouse()
 
 bool Demo::allPlanetsDeployed()
 {
-	for (Planet& plnt : vTotalPlanets)
-		if (!plnt.isDeployed())
+	for (Planet* plnt : vTotalPlanets)
+		if (!plnt->isDeployed())
 			return false;
 
 	return true;
@@ -165,9 +165,9 @@ bool Demo::allPlanetsDeployed()
 
 Planet& Demo::getUndeployedPlanet()
 {
-	for (Planet& plnt : vTotalPlanets)
-		if (!plnt.isDeployed())
-			return plnt;
+	for (Planet* plnt : vTotalPlanets)
+		if (!plnt->isDeployed())
+			return *plnt;
 
 	//WARNING: RETURN CONTROL PATH SHOULD BE FULLY IMPLEMENTED
 }
